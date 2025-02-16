e98510079cec3cb975603af5f0c096e01fd95476
Timothy Qian <12tqian@gmail.com>
2025-02-16 01:38:37 -0500

    finish eval folds

--- src/relearn/attacks/folds.py
+++ src/relearn/attacks/folds.py
@@ -9,8 +9,9 @@ def super_rtt(
     tokenizer: AutoTokenizer,
     records: Dict[str, Dict],
     k_folds: int,
+    lr: float = 5e-5,
+    use_wandb: bool = True,
 ):
-
     folds = get_folds_shuffled(records, k_folds)
 
     for i in range(k_folds):
@@ -18,19 +19,24 @@ def super_rtt(
         train_fold_inds = range(i + 1)
         eval_fold_inds = range(i + 1, k_folds)
 
-        train_dict = {fold_name(i): folds[i]["corpus"] for i in train_fold_inds}
+        train_dict = {
+            f"size_{i+1}_{fold_name(i)}": folds[i]["corpus"] for i in train_fold_inds
+        }
 
-        eval_dict = {fold_name(i): folds[i]["val"] for i in eval_fold_inds}
+        eval_dict = {
+            f"size_{i+1}_{fold_name(i)}": folds[i]["val"] for i in eval_fold_inds
+        }
 
         model = train_rtt(
             model,
             train_dict,
             eval_dict,
-            lr=5e-5,
+            lr=lr,
             tokenizer=tokenizer,
-            use_wandb=True,
+            use_wandb=use_wandb,
             eval_at_start=True if i == 0 else False,
             n_epochs=2,
             max_batches=None,
-            base_epoch=0,
         )
+
+    return model
